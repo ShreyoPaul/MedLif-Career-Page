62228f6ee524122682732c728f2dc8f9f723e8f4
Shreyo Paul <shreyopaul403@gmail.com>
2022-08-31 23:09:09 +0530

    page adding

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
-import Insert from './Component1/Insert';
-// import Apply from './Components/Apply';
-// import Header from './Components/Header';
-
+import Apply from './Components/Apply';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className='page'>
-      {/* <Header/>
-      <Apply/> */}
-      <Insert/>
-
+      <Header/>
+      <Apply/>
     </div>    
   );
 }
